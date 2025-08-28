cebe650fa289e326235ed513707d7fde919f4308
Anyanwu Chukwuemeka <chrisemeka096@gmail.com>
2025-08-28 05:27:09 +0100

    fixed: mismatch naming convection of UserController instead of WalletController

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -1,13 +1,13 @@
-import { UserController } from "../controllers/WalletController";
+import { WalletController } from "../controllers/WalletController";
 import { TransactionController } from "../controllers/TransactionController";
 import { Router } from "express";
 import { authMiddleWare } from "../service/authMiddleware"; 
 
 const router = Router();
 
-router.post('/create-wallet', authMiddleWare, UserController.createWallet);
-router.post('/delete-wallet/:id', authMiddleWare, UserController.deleteWallet);
-router.patch('/update-wallet-status/:id', authMiddleWare, UserController.updateWalletStatus);
+router.post('/create-wallet', authMiddleWare, WalletController.createWallet);
+router.post('/delete-wallet/:id', authMiddleWare, WalletController.deleteWallet);
+router.patch('/update-wallet-status/:id', authMiddleWare, WalletController.updateWalletStatus);
 router.get('/get-balance/:id', authMiddleWare, TransactionController.getBalance);
 
 export default router
