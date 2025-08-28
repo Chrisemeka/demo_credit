eac8d3a5a1cd58d1ac96c18a8d9d77572fd1166f
Anyanwu Chukwuemeka <chrisemeka096@gmail.com>
2025-08-28 01:58:48 +0100

    fixed: updated the database connections

--- knexfile.ts
+++ knexfile.ts
@@ -23,9 +23,14 @@ const config: { [key: string]: Knex.Config } = {
   production: {
     client: "mysql",
     connection: {
-      database: process.env.DB_NAME || 'lendsqr_wallet_dev',
-      user: process.env.DB_USER || 'root',
-      password: ""
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306'),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false 
+      }
     },
     migrations: {
       directory: './src/knexMigrations',
